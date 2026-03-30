345fcf848a0144186c3bd9a823a6a5e100ef3178
niman590 <nimannethmika@gmail.com>
2026-03-30 22:20:24 +0530

    niman

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from routes.prediction_routes import prediction_bp
 from routes.chatbot_routes import chatbot_bp
 from routes.transaction_history_routes import transaction_history_bp
 
-#niman nethmika
+
 app = Flask(__name__)
 app.secret_key = "civic_plan_secret_key"
 
