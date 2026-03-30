b2ce9cacefd21a655a0e9e6ac34dbd23d9ed54db
Thenuk Sandul <mmtsandul@gmail.com>
2026-03-30 23:58:52 +0530

    database

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,12 +1,15 @@
 import sqlite3
 import os
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(base_dir, "land_management_system.db")
+# Get project root directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Build correct database path
+DB_PATH = os.path.join(BASE_DIR, "database", "land_management_system.db")
 
 
 def get_connection():
-    conn = sqlite3.connect(db_path)
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA foreign_keys = ON")
     return conn
