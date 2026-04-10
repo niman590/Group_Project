d985810d8691d0c1ce75c8abb38241a030be6a00
niman590 <nimannethmika@gmail.com>
2026-04-10 23:55:19 +0530

    file seperated

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from routes.main_routes import main_bp
 from routes.auth_routes import auth_bp
 from routes.user_routes import user_bp
 from routes.admin_routes import admin_bp
+from routes.admin_planning_application_routes import admin_planning_bp
 from routes.password_reset_routes import password_reset_bp
 from routes.prediction_routes import prediction_bp
 from routes.chatbot_routes import chatbot_bp
@@ -21,6 +22,7 @@ app.register_blueprint(main_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(user_bp)
 app.register_blueprint(admin_bp)
+app.register_blueprint(admin_planning_bp)
 app.register_blueprint(password_reset_bp)
 app.register_blueprint(prediction_bp)
 app.register_blueprint(chatbot_bp)
